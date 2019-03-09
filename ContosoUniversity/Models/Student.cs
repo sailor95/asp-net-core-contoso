33052691be10a971c58b7f25a1085014af696be2
david.huang <david.huang@getac.com.tw>
2019-03-09 22:56:39 +0800

    [Model] Create seed data Correct FirstMidName in Student Model

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -5,11 +5,11 @@ namespace ContosoUniversity.Models
 {
     public class Student
     {
-        public int ID { get; set;}
-        public string LastName { get; set;}
-        public string FirstName { get; set;}
-        public DateTime EnrollmentDate { get; set;}
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
 
-        public ICollection<Enrollment> Enrollments { get;set;}
+        public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
